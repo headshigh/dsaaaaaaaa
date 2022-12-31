c93f13ac5d347c42aa139f920c42c77007306d3d
headshigh <93933601+headshigh@users.noreply.github.com>
2022-12-31 13:53:40 +0545

    asdf

--- dicerollsum.java
+++ dicerollsum.java
@@ -1,1 +1,20 @@
-public class dicerollsum {}
+import java.util.ArrayList;
+
+public class dicerollsum {
+
+  public static void main(String[] args) {
+    ArrayList<Integer> demo = new ArrayList<>();
+    find(demo, 4, 4);
+  }
+
+  public static void find(ArrayList<Integer> p, int up, int end) {
+    if (up == 0) {
+      System.out.println(p);
+    } else {
+      for (int i = 0; i < up; i++) {
+        p.add(i);
+        find(p, end - i, end);
+      }
+    }
+  }
+}
